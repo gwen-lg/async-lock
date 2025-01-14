6ab9cdf2bdcf5f1b555edea37ae4c852db9a408b
Gwen Lg <me@gwenlg.fr>
2025-01-14 01:01:27 +0100

    tried implement it without Event management

--- src/refcell.rs
+++ src/refcell.rs
@@ -1,5 +1,5 @@
 //! source of inspiration :
-//! https://rust-lang.github.io/async-book/02_execution/03_wakeups.html
+//! <https://rust-lang.github.io/async-book/02_execution/03_wakeups.html>
 
 mod raw;
 
@@ -7,18 +7,18 @@ use core::{
     cell::UnsafeCell,
     fmt,
     future::Future,
-    ops::Deref,
+    ops::{Deref, DerefMut},
     ptr::NonNull,
-    task::{Poll, Waker},
+    task::Poll,
 };
-use raw::{RawBorrow, RawRefCell};
+use raw::{RawBorrow, RawBorrowMut, RawRefCell};
 
-#[derive(Clone, Copy, Debug, PartialEq, Eq)]
-enum BorrowFlag {
-    Available,
-    Read { count: u32 },
-    Write,
-}
+// #[derive(Clone, Copy, Debug, PartialEq, Eq)]
+// enum BorrowFlag {
+//     Available,
+//     Read { count: u32 },
+//     Write,
+// }
 /// TODO: document
 pub struct RefCell<T: ?Sized> {
     //borrow: Cell<BorrowFlag>,
@@ -26,6 +26,7 @@ pub struct RefCell<T: ?Sized> {
     value: UnsafeCell<T>,
 }
 impl<T> RefCell<T> {
+    /// Create a new RefCell.
     pub fn new(value: T) -> RefCell<T> {
         Self {
             raw: RawRefCell::new(),
@@ -36,7 +37,7 @@ impl<T> RefCell<T> {
 }
 
 impl<T: ?Sized> RefCell<T> {
-    ///TODO:
+    //TODO:
     // pub fn into_inner(self) -> T {
     //     self.value.into_inner()
     // }
@@ -45,11 +46,18 @@ impl<T: ?Sized> RefCell<T> {
     //pub fn replace_with<F>(&self, f: F) -> T
     //pub fn swap(&self, other: &RefCell<T>)
 
+    /// Acquire a borrow on the wrapped value.
     ///
-    pub fn borrow(&self) -> Borrow<T> {
+    /// This wait the end of the current borrow_mut.
+    ///
+    /// Returns a guard that releases the lock when dropped.
+    pub fn borrow(&self) -> Borrow<'_, T> {
         Borrow::new(self.raw.borrow(), self.value.get())
     }
 
+    /// Tried to acquire a borrow on the wrapped value.
+    ///
+    /// Return `None` instead of wait if a borrow_mut is in progress.
     pub fn try_borrow(&self) -> Option<Ref<'_, T>> {
         if self.raw.try_borrow() {
             Some(Ref {
@@ -61,9 +69,26 @@ impl<T: ?Sized> RefCell<T> {
         }
     }
 
-    // /// Mutably borrows the wrapped value.
-    // /// await for the resource availability if needed.
-    //pub async fn borrow_mut(&self) -> RefMut<'_, T> {}
+    /// Mutably borrows the wrapped value.
+    /// `await` if the value is currently borrowed. For non `async` variant, use [`try_borrow_mut`].
+    /// TODO:
+    pub fn borrow_mut(&self) -> BorrowMut<'_, T> {
+        BorrowMut::new(self.raw.borrow_mut(), self.value.get())
+    }
+
+    /// Tried to acquire a borrow on the wrapped value.
+    ///
+    /// Return `None` instead of wait if a borrow_mut is in progress.
+    pub fn try_borrow_mut(&self) -> Option<RefMut<'_, T>> {
+        if self.raw.try_borrow_mut() {
+            Some(RefMut {
+                value: self.value.get(),
+                lock: &self.raw,
+            })
+        } else {
+            None
+        }
+    }
 }
 
 impl<T: fmt::Debug + ?Sized> fmt::Debug for RefCell<T> {
@@ -98,7 +123,7 @@ impl<'b, T: ?Sized + 'b> Future for Borrow<'b, T> {
     type Output = Ref<'b, T>;
 
     fn poll(
-        mut self: core::pin::Pin<&mut Self>,
+        self: core::pin::Pin<&mut Self>,
         cx: &mut core::task::Context<'_>,
     ) -> Poll<Self::Output> {
         if self.raw.try_borrow() {
@@ -107,6 +132,8 @@ impl<'b, T: ?Sized + 'b> Future for Borrow<'b, T> {
                 value: self.value.as_ptr(),
             })
         } else {
+            // set state waiting ?
+            self.raw.lock.borrow_wake(cx.waker().clone());
             Poll::Pending
         }
     }
@@ -118,7 +145,50 @@ impl<T: ?Sized> fmt::Debug for Borrow<'_, T> {
         f.write_str("Borrow { .. }")
     }
 }
+pub struct BorrowMut<'b, T: ?Sized> {
+    value: NonNull<T>,
+    raw: RawBorrowMut<'b>, // &'b
+                           //waker: Waker,
+}
 
+impl<'x, T: ?Sized> BorrowMut<'x, T> {
+    fn new(raw: RawBorrowMut<'x>, value: *mut T) -> Self {
+        let value = unsafe { NonNull::new_unchecked(value) };
+        Self { value, raw }
+    }
+}
+
+impl<'b, T: ?Sized + 'b> Future for BorrowMut<'b, T> {
+    type Output = RefMut<'b, T>;
+
+    fn poll(
+        self: core::pin::Pin<&mut Self>,
+        cx: &mut core::task::Context<'_>,
+    ) -> Poll<Self::Output> {
+        if self.raw.try_borrow_mut() {
+            Poll::Ready(RefMut::<T> {
+                lock: self.raw.lock,
+                value: self.value.as_ptr(),
+            })
+        } else {
+            // set state waiting ?
+            self.raw.lock.borrow_mut_wake(cx.waker().clone());
+            Poll::Pending
+        }
+    }
+}
+
+impl<T: ?Sized> fmt::Debug for BorrowMut<'_, T> {
+    #[inline]
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.write_str("BorrowMut { .. }")
+    }
+}
+
+/// Wraps a borrowed reference to a value in a `RefCell` box.
+/// A wrapper type for an immutably borrowed value from a `RefCell<T>`.
+///
+/// See the [module-level documentation](self) for more.
 pub struct Ref<'a, T: ?Sized + 'a> {
     lock: &'a RawRefCell,
     value: *const T,
@@ -127,10 +197,7 @@ pub struct Ref<'a, T: ?Sized + 'a> {
 impl<T: ?Sized> Drop for Ref<'_, T> {
     #[inline]
     fn drop(&mut self) {
-        // SAFETY: we are dropping a read guard.
-        //unsafe {
         self.lock.borrow_unlock();
-        //}
     }
 }
 impl<T: fmt::Debug + ?Sized> fmt::Debug for Ref<'_, T> {
@@ -154,4 +221,43 @@ impl<T: ?Sized> Deref for Ref<'_, T> {
     }
 }
 
-// //pub struct RefMut<'b, T: ?Sized + 'b> {}
+/// A wrapper type for a mutably borrowed value from a RefCell<T>.
+///
+/// See the [module-level documentation](self) for more.
+pub struct RefMut<'a, T: ?Sized + 'a> {
+    lock: &'a RawRefCell,
+    value: *mut T,
+}
+
+impl<T: ?Sized> Drop for RefMut<'_, T> {
+    #[inline]
+    fn drop(&mut self) {
+        self.lock.borrow_mut_unlock();
+    }
+}
+impl<T: fmt::Debug + ?Sized> fmt::Debug for RefMut<'_, T> {
+    #[inline]
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        fmt::Debug::fmt(&**self, f)
+    }
+}
+impl<T: fmt::Display + ?Sized> fmt::Display for RefMut<'_, T> {
+    #[inline]
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        (**self).fmt(f)
+    }
+}
+impl<T: ?Sized> Deref for RefMut<'_, T> {
+    type Target = T;
+
+    #[inline]
+    fn deref(&self) -> &T {
+        unsafe { &*self.value }
+    }
+}
+impl<T: ?Sized> DerefMut for RefMut<'_, T> {
+    #[inline]
+    fn deref_mut(&mut self) -> &mut T {
+        unsafe { &mut *self.value }
+    }
+}
--- src/refcell/raw.rs
+++ src/refcell/raw.rs
@@ -1,16 +1,20 @@
-use core::sync::atomic::{AtomicUsize, Ordering};
-
-use event_listener::{Event, EventListener};
+use core::{
+    cell::{Cell, RefCell},
+    task::Waker,
+};
+use std::collections::VecDeque;
 
 const BORROW_MUT_BIT: usize = 1;
 const ONE_BORROW: usize = 2;
 
 pub(super) struct RawRefCell {
     /// Event triggered when the last borrow is dropped.
-    no_borrows: Event,
+    //no_borrows: Event,
+    borrow_wakes: RefCell<VecDeque<Waker>>,
 
     /// Event triggered when the borrow_mut is dropped.
-    no_borrow_mut: Event,
+    //no_borrow_mut: Event,
+    borrow_mut_wakes: RefCell<VecDeque<Waker>>,
 
     /// Current state of the lock.
     ///
@@ -19,58 +23,116 @@ pub(super) struct RawRefCell {
     ///
     /// The upper bits contain the number of currently active readers. Each active reader
     /// increments the state by `ONE_READER`.
-    state: AtomicUsize,
+    //state: AtomicUsize,
+    state: Cell<usize>,
 }
 
 impl RawRefCell {
     pub(super) const fn new() -> Self {
         Self {
-            no_borrows: Event::new(),
-            no_borrow_mut: Event::new(),
-            state: AtomicUsize::new(0),
+            //no_borrows: Event::new(),
+            //no_borrow_mut: Event::new(),
+            //state: AtomicUsize::new(0),
+            borrow_wakes: RefCell::new(VecDeque::new()),
+            borrow_mut_wakes: RefCell::new(VecDeque::new()),
+            state: Cell::new(0),
         }
     }
 
     pub(super) fn borrow(&self) -> RawBorrow<'_> {
         RawBorrow {
             lock: self,
-            state: self.state.load(Ordering::Acquire), //TODO: no need atomic as single threaded ?
-            listener: None,
+            //state: self.state.get(), //TODO: no need atomic as single threaded ?
+            //listener: None,
         }
     }
 
+    pub(super) fn borrow_wake(&self, waker: Waker) {
+        self.borrow_wakes.borrow_mut().push_back(waker);
+    }
+
     pub(super) fn try_borrow(&self) -> bool {
-        let mut state = self.state.load(Ordering::Acquire);
-        loop {
-            // If there's a mutable borrow holding the lock or attempting to acquire it, we cannot acquire
-            // a read lock here.
-            if state & BORROW_MUT_BIT != 0 {
-                return false;
-            }
+        let state = self.state.get();
 
-            // Make sure the number of borrows doesn't overflow.
-            if state > isize::MAX as usize {
-                crate::abort();
-            }
-
-            // Increment the number of readers.
-            match self.state.compare_exchange(
-                state,
-                state + ONE_BORROW,
-                Ordering::AcqRel,
-                Ordering::Acquire,
-            ) {
-                Ok(_) => return true,
-                Err(s) => state = s,
-            }
+        // If there's a mutable borrow holding the lock or attempting to acquire it, we cannot acquire
+        // a read lock here.
+        if state & BORROW_MUT_BIT != 0 {
+            return false;
         }
+
+        // Make sure the number of borrows doesn't overflow.
+        if state > isize::MAX as usize {
+            crate::abort();
+        }
+
+        // Increment the number of readers.
+        // TODO ? ok if self.state.update(|val| val + ONE_BORROW ) == state + ONE_BORROW;
+        self.state.set(state + ONE_BORROW);
+
+        true
     }
 
     pub(super) fn borrow_unlock(&self) {
         // Decrement the number of borrows.
-        if self.state.fetch_sub(ONE_BORROW, Ordering::SeqCst) & !BORROW_MUT_BIT == ONE_BORROW {
-            // If this was the last reader, trigger the "no borrows" event.
-            self.no_borrows.notify(1);
+        let state = self.state.get();
+        self.state.set(state - ONE_BORROW);
+
+        if self.state.get() == 0 {
+            // If this was the last reader, wake up the next borrow the "no borrows" event.
+            if let Some(borrow_mut_wake) = self.borrow_mut_wakes.borrow_mut().pop_front() {
+                borrow_mut_wake.wake();
+            }
+        }
+    }
+
+    pub(super) fn borrow_mut(&self) -> RawBorrowMut<'_> {
+        RawBorrowMut {
+            lock: self,
+            //state: self.state.get(), //TODO: no need atomic as single threaded ?
+            //listener: None,
+        }
+    }
+
+    pub(super) fn borrow_mut_wake(&self, waker: Waker) {
+        self.borrow_mut_wakes.borrow_mut().push_back(waker);
+    }
+
+    pub(super) fn try_borrow_mut(&self) -> bool {
+        let state = self.state.get();
+
+        // If there's a mutable borrow holding the lock or attempting to acquire it, we cannot acquire
+        // a borrow_mut lock here.
+        if state & BORROW_MUT_BIT != 0 {
+            return false;
+        }
+
+        // If there's at least one simple borrow, we cannot acquire
+        // a borrow_mut lock here.
+        if state & !BORROW_MUT_BIT != 0 {
+            return false;
+        }
+
+        // Increment the number of readers.
+        // TODO ? ok if self.state.update(|val| val & BORROW_MUT_BIT ) == BORROW_MUT_BIT;
+        self.state.set(BORROW_MUT_BIT);
+
+        true
+    }
+
+    pub(super) fn borrow_mut_unlock(&self) {
+        if let Some(borrow_mut_wake) = self.borrow_mut_wakes.borrow_mut().pop_front() {
+            // If there is a waiting borrow_mut, wake up
+            borrow_mut_wake.wake();
+        } else {
+            // Only remove Borrow mut bit, if there is no other task waiting for it.
+            let new_state = self.state.get() & !BORROW_MUT_BIT;
+            self.state.set(new_state);
+            // else, wakeup borrow_wake
+            let mut borrow_wakes = self.borrow_wakes.borrow_mut();
+
+            borrow_wakes
+                .drain(0..)
+                .for_each(|waiting_borrow| waiting_borrow.wake());
         }
     }
 }
@@ -78,43 +140,62 @@ impl RawRefCell {
 pub(super) struct RawBorrow<'a> {
     // The lock that is being acquired.
     pub(super) lock: &'a RawRefCell,
-
     // The listener for the "no writers" event.
-    listener: Option<EventListener>,
+    //listener: Option<EventListener>,
 
     // The last-observed state of the lock.
-    state: usize,
+    //state: usize,
     // // Making this type `!Unpin` enables future optimizations.
     // #[pin]
     // _pin: PhantomPinned
 }
 
 impl RawBorrow<'_> {
-    pub fn try_borrow(&mut self) -> bool {
+    pub fn try_borrow(&self) -> bool {
+        self.lock.try_borrow()
         //let this = self.project();
-        if self.state & BORROW_MUT_BIT == 0 {
-            // Make sure the number of readers doesn't overflow.
-            if self.state > isize::MAX as usize {
-                crate::abort();
-            } else {
-                match self.lock.state.compare_exchange(
-                    self.state,
-                    self.state + ONE_BORROW,
-                    Ordering::AcqRel,
-                    Ordering::Acquire,
-                ) {
-                    Ok(_) => return true,
-                    Err(s) => self.state = s,
-                }
-            }
-        } else {
-            if self.listener.is_none() {
-                self.listener = Some(self.lock.no_borrows.listen())
-            } else {
-                todo!()
-            }
-            self.state = self.lock.state.load(Ordering::Acquire);
-        }
-        false
+        // if self.state & BORROW_MUT_BIT == 0 {
+        //     // Make sure the number of readers doesn't overflow.
+        //     if self.state > isize::MAX as usize {
+        //         crate::abort();
+        //     } else {
+        //         match self.lock.state.compare_exchange(
+        //             self.state,
+        //             self.state + ONE_BORROW,
+        //             Ordering::AcqRel,
+        //             Ordering::Acquire,
+        //         ) {
+        //             Ok(_) => return true,
+        //             Err(s) => self.state = s,
+        //         }
+        //     }
+        // } else {
+        //     if self.listener.is_none() {
+        //         self.listener = Some(self.lock.no_borrows.listen())
+        //     } else {
+        //         todo!()
+        //     }
+        //     self.state = self.lock.state.load(Ordering::Acquire);
+        // }
+        // false
+    }
+}
+
+pub(super) struct RawBorrowMut<'a> {
+    // The lock that is being acquired.
+    pub(super) lock: &'a RawRefCell,
+    // The listener for the "no writers" event.
+    //listener: Option<EventListener>,
+
+    // The last-observed state of the lock.
+    //state: usize,
+    // // Making this type `!Unpin` enables future optimizations.
+    // #[pin]
+    // _pin: PhantomPinned
+}
+
+impl RawBorrowMut<'_> {
+    pub fn try_borrow_mut(&self) -> bool {
+        self.lock.try_borrow_mut()
     }
 }
